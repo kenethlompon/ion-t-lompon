ae921c0299aa53bf45deead14d294bc29ea8aee2
kenethlompon <lomponkeneth02@gmail.com>
2024-03-19 08:10:33 +0800

    On App.tsx Added home Outline

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { calculatorOutline, ellipse, nuclearOutline, personCircleOutline, square, triangle } from 'ionicons/icons';
+import { calculatorOutline, ellipse, homeOutline, nuclearOutline, personCircleOutline, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -60,7 +60,7 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
         <IonTabButton tab="tab1" href="/Home">
-            <IonIcon aria-hidden="true" icon={personCircleOutline} />
+            <IonIcon aria-hidden="true" icon={homeOutline} />
             <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab1" href="/tab1">
