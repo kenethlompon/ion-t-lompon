6d50b08e7b50235d027e10697164ccc1e2756091
kenethlompon <lomponkeneth02@gmail.com>
2024-04-16 09:00:24 +0800

    added changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,57 @@
-import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
-} from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon } from '@ionic/react';
+import { calculator } from 'ionicons/icons';
+import './ClickCounter.css';
 
-// CSS
-//import './profile.css';
+const ClickCounter: React.FC = () => {
+  const [count, setCount] = useState(0);
 
-  const Home: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Click Counter</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <IonButton onClick={() => setCount(count + 1)}>
+          Click me!
+        </IonButton>
+        <p>You have clicked {count} times.</p>
+      </IonCardContent>
+    </IonCard>
+  );
+};
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle>Home</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Home</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-        </IonContent>
-      </IonPage>
-    );
-  };
+        <IonCard>
+          <IonCardContent>
+            <IonButton routerLink="/click-counter">Click Counter</IonButton>
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardContent>
+            <IonButton routerLink="/calculator">
+              <IonIcon icon={calculator} />
+              Calculator
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-  //
-  export default Home;
+export default Home;
+export { ClickCounter };
